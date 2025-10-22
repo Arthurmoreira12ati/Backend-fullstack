8d1fa7a183de4c2cfbd0ddef8d15cfdc18977a5c
Arthur moreira <arthurmsouza416@gmail.com>
2025-10-21 23:50:10 -0300

    backend

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express, { response } from "express"
+import cors from "cors"
+import { people } from "./people.js"
+
+const app = express()
+const port = 3333
+
+app.use(cors())
+app.get("/", (request, response) => { 
+    response.json(people)
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`)
+})
